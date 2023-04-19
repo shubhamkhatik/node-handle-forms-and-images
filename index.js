e512c0b30035ed478d449e02195bfbcdd03bb7fb
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2023-04-19 14:29:45 +0530

    multiple image

--- index.js
+++ index.js
@@ -74,3 +74,40 @@ app.get("/postform", (req, res) => {
 });
 
 app.listen(4000, () => console.log(`Server is runnning at port 4000`));
+// ========================code for accept multiple images=============================
+
+app.post("/mypostmulti", async (req, res) => {
+let result;
+let imageArray = [];
+
+// case - multiple images
+
+if (req.files) {
+  for (let index = 0; index < req.files.samplefile.length; index++) {
+    let result = await cloudinary.uploader.upload(
+      req.files.samplefile[index].tempFilePath,
+      {
+        folder: "images",
+      }
+    );
+
+    imageArray.push({
+      public_id: result.public_id,
+      secure_url: result.secure_url,
+    });
+  }
+}
+
+
+console.log(result);
+
+details = {
+  firstname: req.body.firstname,
+  lastname: req.body.lastname,
+  result,
+  imageArray,
+};
+console.log(details);
+
+res.send(details);
+});
